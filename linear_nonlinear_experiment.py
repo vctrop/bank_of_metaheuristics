9450419908cd542303314ca21e6cd818571727ed
Victor O. Costa <victor-o.c@hotmail.com>
2020-05-18 09:50:37 -0300

    Implement AGD and ACS running N times and statistical measures extraction. Also silence SMAC

--- linear_nonlinear_experiment.py
+++ linear_nonlinear_experiment.py
@@ -15,10 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 # Python standard library
 import math
 import sys
+import io
 # Own
 from ant_colony_for_continuous_domains import ACSACOr, AGDACOr
 ## 3rd party
@@ -26,15 +26,15 @@ import numpy as np
 # Benchmarking functions
 from deap.benchmarks import rosenbrock, schwefel, ackley, griewank, himmelblau      # Load functions used as train instances
 # SMAC
-from ConfigSpace.hyperparameters import UniformFloatHyperparameter
-from smac.configspace import ConfigurationSpace
-from smac.facade.smac_hpo_facade  import SMAC4HPO
-from smac.scenario.scenario import Scenario
+# from ConfigSpace.hyperparameters import UniformFloatHyperparameter
+# from smac.configspace import ConfigurationSpace
+# from smac.facade.smac_hpo_facade  import SMAC4HPO
+# from smac.scenario.scenario import Scenario
 
 
 # Given a function, an adaptive mechanism for ACOr, parameters and linearity of adaptive parameter control,
 #  return the average results of a number runs on a suite of functions
-def function_cost(function, variables_range, bounded, acor_mechanism, minimum, maximum, linear_control):
+def function_cost(function, variables_range, bounded, acor_mechanism, minimum, maximum, linear_control, num_iterations):
     if not isinstance(acor_mechanism, str):
         print("Error, ACOr mechanism must be a string")
         exit(-1)
@@ -47,7 +47,6 @@ def function_cost(function, variables_range, bounded, acor_mechanism, minimum, m
         
     # Base ACOr parameters, from (Socha, 2008)
     # Number of function evaluations (F.E.) = k + iterations * m
-    iterations = 175    # 175 iterations = 400 F.E.
     k = 50
     m = 2    
     q = 1e-4
@@ -56,11 +55,11 @@ def function_cost(function, variables_range, bounded, acor_mechanism, minimum, m
     if acor_mechanism == "ACS":
         colony = ACSACOr()
         colony.set_verbosity(False)
-        colony.set_parameters(iterations, m, k, xi, minimum, maximum, linear_control)
+        colony.set_parameters(num_iterations, m, k, xi, minimum, maximum, linear_control)
     else:
         colony = AGDACOr()  
         colony.set_verbosity(False)
-        colony.set_parameters(iterations, m, k, q, minimum, maximum, linear_control)
+        colony.set_parameters(num_iterations, m, k, q, minimum, maximum, linear_control)
     
     # Define ranges and bounding of each variable
     dimensionality = 2                     # Number of variables for all functions
@@ -80,7 +79,7 @@ def define_smac_cost(train_functions, functions_names, functions_bounding, funct
         if len(train_functions) == 0:
             print('Error, there must be at least one function in the train functions list')
             exit(-1)
-        if len(train_functions) != len(function_names):
+        if len(train_functions) != len(functions_names):
             print('Error, every objective function must be associated with a name string')
             exit(-1)
         
@@ -91,11 +90,14 @@ def define_smac_cost(train_functions, functions_names, functions_bounding, funct
         if maximum >= 1:
             maximum = 0.99
         
+        # Number of function evaluations (F.E.) = k + iterations * m
+        acor_iterations = 175    # 175 iterations = 400 F.E.
+        
         total_cost = 0.0
         for objective_function, function_str in zip(train_functions, functions_names):
             variables_bounded   = functions_bounding[function_str]
             variables_range     = functions_ranges[function_str]
-            total_cost          += function_cost(objective_function, variables_range, variables_bounded, acor_mechanism, minimum, maximum, linear_control)
+            total_cost          += function_cost(objective_function, variables_range, variables_bounded, acor_mechanism, minimum, maximum, linear_control, acor_iterations)
         
         return total_cost / len(train_functions)
         
@@ -105,7 +107,7 @@ def extract_linear_nonlinear_results():
     train_functions = [rosenbrock, schwefel, ackley, griewank, himmelblau]
     train_functions_names = ['rosenbrock', 'schwefel','ackley','griewank','himmelblau']
     mechanisms = ['ACS', 'AGD']               
-    func_evals_smac = 200
+    func_evals_smac = 1000
     
     functions_bounding = {'rosenbrock':  False,
                          'schwefel':    True, 
@@ -113,7 +115,7 @@ def extract_linear_nonlinear_results():
                          'griewank':    True, 
                          'himmelblau':  True}
                             
-    functions_ranges = {'rosenbrock':    [-10, 10],   # unbounded, initialization only
+    functions_ranges = {'rosenbrock':   [-10, 10],   # unbounded, initialization only
                        'schwefel':      [-500, 500],
                        'ackley':        [-15, 30],  
                        'griewank':      [-600, 600],
@@ -148,7 +150,59 @@ def extract_linear_nonlinear_results():
             smac = SMAC4HPO(scenario    = scenario,
                             rng         = np.random.RandomState(42),
                             tae_runner  = define_smac_cost(train_functions, train_functions_names, functions_bounding, functions_ranges, mechanism, use_linear))
-
+            
+            # Save stdout and keep it frozen, to silence SMAC
+            save_stdout = sys.stdout
+            sys.stdout = io.BytesIO()
+            # Run SMAC to optimize the maximum and minimum values of q and xi, for ACS and AGD, respectively.
             solution = smac.optimize()
+            # Restore stdout
+            sys.stdout = save_stdout
+            
             
             np.save('./results/linear_nonlinear/' + linearity_str + '_'+ mechanism + '.npy', smac_solutions)
+
+            
+# Read 4 solutions generated by SMAC, one for each mechanism-linearity combination
+def run_lin_nlin_smac_params():
+    
+    metaheuristic_runs = 100
+    mechanisms = ['ACS', 'AGD']    
+    train_functions = [rosenbrock, schwefel, ackley, griewank, himmelblau]
+    train_functions_names = ['rosenbrock', 'schwefel','ackley','griewank','himmelblau']
+    functions_bounding = {'rosenbrock':  False,
+                         'schwefel':    True, 
+                         'ackley':      True, 
+                         'griewank':    True, 
+                         'himmelblau':  True}
+                            
+    functions_ranges = {'rosenbrock':   [-10, 10],   # unbounded, initialization only
+                       'schwefel':      [-500, 500],
+                       'ackley':        [-15, 30],  
+                       'griewank':      [-600, 600],
+                       'himmelblau':    [-6,6]} 
+
+    for linearity, linearity_str in zip([True, False],['linear', 'nonlinear']):
+        for mechanism in mechanisms:
+            # Load solution generated by SMAC for a given mechanism-linearity combination
+            smac_solution = np.load('./results/linear_nonlinear/' + linearity_str + '_'+ mechanism + '.npy')
+            print(smac_solution)
+            minimum = smac_solution['min']
+            maximum = minimum + smac_solution['max_minus_min']
+            if maximum >= 1:
+                maximum = 0.99
+            
+            acor_iterations = 475       # 475 * 2 + 50 = 1000 F.E. 
+            
+            # For each train function, run a metaheuristic N times and save results
+            for function, function_str in zip(train_functions, train_functions_names):        
+                variables_bounded = functions_bounding[function_str]
+                variables_range = functions_ranges[function_str]
+                
+                # Run metaheuristic N times
+                function_costs = []
+                for _ in range(metaheuristic_runs):
+                    function_costs.append(function_cost(function, variables_range, variables_bounded, acor_mechanism, minimum, maximum, linear_control, acor_iterations))
+                np.save('./results/linear_nonlinear/' + linearity_str + '_'+ mechanism + '_' + function_str + '_eval.npy', function_costs)
+                
+run_lin_nlin_smac_params()
---
+++ linear_nonlinear_results_stats.py
@@ -0,0 +1,56 @@
+#!python3
+
+# Copyright (C) 2020  Victor O. Costa
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+from scipy.stats import wilcoxon
+import linear_nonlinear_experiment
+
+# Print average and standard deviation of all metaheuristic applications for a given SMAC solution 
+def print_avg_std():
+    mechanisms = ['ACS', 'AGD']
+    function_names = ['rosenbrock', 'schwefel','ackley','griewank','himmelblau']
+    
+    for mechanism in mechanisms:
+        print(mechanism + 'ACOr')
+        
+        for function_str in function_names:  
+            print(function_str.upper())
+        
+            for linearity_str in ['linear', 'nonlinear']:
+                print(linearity_str)
+                function_costs = np.load('./results/linear_nonlinear/' + linearity_str + '_'+ mechanism + '_' + function_str + '_eval.npy')
+                mean = np.mean(function_costs)
+                std  = np.std(function_costs)
+                print(mean + ' (' + std +')')
+            
+            
+# Print average and standard deviation of all metaheuristic applications for a given SMAC solution 
+def print_p_values():
+    mechanisms = ['ACS', 'AGD']
+    function_names = ['rosenbrock', 'schwefel','ackley','griewank','himmelblau']
+    
+    for mechanism in mechanisms:
+        print(mechanism + 'ACOr')
+        
+        for function_str in function_names:  
+            print(function_str.upper())
+            
+            linear_costs    = np.load('./results/linear_nonlinear/linear' + '_'+ mechanism + '_' + function_str + '_eval.npy')
+            nonlinear_costs = np.load('./results/linear_nonlinear/nonlinear' + '_'+ mechanism + '_' + function_str + '_eval.npy')
+            _, pval = wilcoxon(linear_costs, nonlinear_costs)
+            print('Linear vs nonlinear control mechanism p-value' + str(pval))
+            
