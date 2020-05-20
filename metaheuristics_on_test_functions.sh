4d9e911cf88a338ea7e55d1b31ec03408c5c6f0c
Victor O. Costa <victor-o.c@hotmail.com>
2020-05-20 19:53:31 -0300

    Upload results of lin v sig v exp

--- metaheuristics_on_test_functions.sh
+++ metaheuristics_on_test_functions.sh
@@ -1,7 +1,7 @@
 # Each run of metaheuristic_test_functions_experiment.py returns a dictionary with 8x100 matrices (100 runs for each number of F.E.) for each test function key  
-nohup python3 metaheuristic_test_functions_experiment.py "acor"
-nohup python3 metaheuristic_test_functions_experiment.py "acsacor"
-nohup python3 metaheuristic_test_functions_experiment.py "agdacor"
-nohup python3 metaheuristic_test_functions_experiment.py "maacor"
-nohup python3 metaheuristic_test_functions_experiment.py "aiwpso"
-nohup python3 metaheuristic_test_functions_experiment.py "acfsa"
+nohup python3 metaheuristic_test_functions_experiment.py acor
+nohup python3 metaheuristic_test_functions_experiment.py acsacor
+nohup python3 metaheuristic_test_functions_experiment.py agdacor
+nohup python3 metaheuristic_test_functions_experiment.py maacor
+nohup python3 metaheuristic_test_functions_experiment.py aiwpso
+nohup python3 metaheuristic_test_functions_experiment.py acfsa
