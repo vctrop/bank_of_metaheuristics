d47a967f3bc3445b2452f806a194ef5827f94b4b
Victor O. Costa <victor-o.c@hotmail.com>
2020-05-22 12:47:35 -0300

    Reindexing in simulated annealing cost function calling

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -117,7 +117,7 @@ class SA(Base):
         for i in range(self.num_variables):
             self.current_solution[i] = np.random.uniform(self.initial_ranges[i][0], self.initial_ranges[i][1])
         # Compute its cost considering that weights were modified
-        self.current_solution[-1] = self.cost_function(self.current_solution)
+        self.current_solution[-1] = self.cost_function(self.current_solution[:-1])
         self.best_solution = np.array(self.current_solution)
 
         # Keep solutions defined by function_evaluations_array
@@ -154,7 +154,7 @@ class SA(Base):
                 
                 candidate_solution = np.array(self.current_solution)
                 candidate_solution[self.chosen_variable] = pertubated_variable
-                candidate_solution[-1] = self.cost_function(candidate_solution)
+                candidate_solution[-1] = self.cost_function(candidate_solution[:-1])
                 
                 # Decide if solution will replace the current one based on the Metropolis sampling algorithm
                 delta_J = candidate_solution[-1] - self.current_solution[-1] 
